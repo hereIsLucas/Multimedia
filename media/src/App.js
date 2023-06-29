0c5517a720ac3b88cf0d70204d6dfadc73a23396
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-06-29 20:28:27 +0200

    add: file with pictures

--- media/src/App.js
+++ media/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import "./App.css"
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <div id="picture"></div>
     </div>
   )
 }
