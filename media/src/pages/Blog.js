5b10fdb8649d98e0817a77acad294d54c756afdd
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-06-30 12:19:30 +0200

    add: feature, now you can zoom into the picture

--- media/src/pages/Blog.js
+++ media/src/pages/Blog.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Blog.css';
 import images from '../images';
 
-
 export default function Blog() {
-    return (
-        <div id='body'>
-            <div className="blog">
-                {images.map((image, index) => (
-                    <div className="blog-item" key={index}>
-                        <img src={image} alt={'Image ${index + 1}'} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const toggleExpand = (index) => {
+    if (expandedIndex === index) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(index);
+    }
+  };
+
+  return (
+    <div id='body'>
+      <div className="blog">
+        {images.map((image, index) => (
+          <div
+            className={`blog-item ${expandedIndex === index ? 'expanded' : 'collapsed'}`}
+            key={index}
+            onClick={() => toggleExpand(index)}
+          >
+            <img src={image} alt={`Image ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
