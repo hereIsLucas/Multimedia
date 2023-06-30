6f44f812ccfaa9cc14e39978a22fa79c4696fe85
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-06-30 04:01:14 +0200

    add: about me page

--- media/src/pages/AboutMe.js
+++ media/src/pages/AboutMe.js
@@ -1,21 +1,41 @@
 import React from 'react'
+import './AboutMe.css';
 
-export default function AboutMe() {
+
+export default function AboutUs() {
     return (
         <div>
-            <h1>Test</h1>
-            <p>hello</p>
-            <p>hello</p>
+            <div id='body'>
+                <h1>About Me</h1>
+                <p>
+                    Hello, I'm Lucas! I am a passionate and dedicated photographer based in [Your Location]. Photography is not just a hobby for me, but a way of life. It allows me to capture the beauty and essence of the world around us, preserving precious moments in time.
+                </p>
+                <br />
+                <p>
+                    My love for photography started at a young age, and over the years, it has grown into a deep-rooted passion. I am constantly inspired by the power of visual storytelling and the ability of photographs to evoke emotions and ignite imagination.
+                </p>
+                <br />
 
-            <p>hello</p>
+                <p>
+                    While I enjoy capturing a wide range of subjects, my current focus is on exploring the enchanting landscapes and rich cultural heritage of Indonesia. In 2022, I had the incredible opportunity to embark on a photographic journey to Bali, Indonesia, where I immersed myself in the vibrant colors, captivating landscapes, and warm hospitality of this beautiful country.
+                </p>
+                <br />
 
-            <p>hello</p>
+                <p>
+                    When it comes to my photography style, I believe in a combination of artistry and technical excellence. I strive to create images that not only showcase the beauty of the subject but also convey a sense of mood, emotion, and narrative. Each photograph is a carefully crafted visual story, capturing the essence of the moment and inviting viewers to embark on their own journey of interpretation.
+                </p>
+                <br />
 
-            <p>hello</p>
+                <p>
+                    Through my work, I aim to inspire others to appreciate the world around them, to see the extraordinary in the ordinary, and to find beauty in the smallest of details. I hope that my photographs serve as a source of inspiration, allowing viewers to pause, reflect, and connect with the world in a deeper, more meaningful way.
+                </p>
+                <br />
 
-            <p>hello</p>
-
-
+                <p>
+                    Join me on this incredible visual adventure as I continue to explore new horizons, push the boundaries of creativity, and share my passion for photography with the world. Together, let's celebrate the magic of images and the infinite stories they hold.
+                </p>
+            </div>
         </div>
+
     )
 }
--- media/src/pages/Blog.js
+++ media/src/pages/Blog.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Blog() {
     return (
         <div>
+            <div>
+                
+            </div>
             <h1>Test</h1>
             <p>hello</p>
             <p>hello</p>
